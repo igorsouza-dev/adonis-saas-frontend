f9917de82eb8409fd4460dbdd24c5045e195d2fd
Igor Souza <igorsouza.dev@gmail.com>
2020-01-02 16:46:40 -0300

    Added global style and styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import GlobalStyle from 'styles/global';
 import Routes from './routes';
 import store from './store';
 
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
   </Provider>
 );
 
