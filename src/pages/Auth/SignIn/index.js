c1be3c990bec09bbfb274f5e76982f3f5d83ed24
Igor Souza <igorsouza.dev@gmail.com>
2020-01-02 20:30:21 -0300

    Added signin style

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Button from 'styles/components/Button';
+import { Container, SignForm } from '../styles';
 
-// import { Container } from './styles';
+const SignIn = () => {
+  function handleSubmit() {}
+  return (
+    <Container>
+      <SignForm onSubmit={handleSubmit}>
+        <h1>Welcome!</h1>
 
-const SignIn = () => <div>Sign In</div>;
+        <span>E-MAIL</span>
+        <input type="text" name="email" />
+
+        <span>PASSWORD</span>
+        <input type="password" name="password" />
+        <Button size="big" type="submit">
+          Signin
+        </Button>
+      </SignForm>
+    </Container>
+  );
+};
 
 export default SignIn;
